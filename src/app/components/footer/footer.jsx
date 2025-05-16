7d610a4dbda1aea8a00fc2d1d697475459adec04
justKBW <awam_gesch@web.de>
2025-05-16 21:54:42 +0200

    Added counter increment in footer-component

--- src/app/components/footer/footer.jsx
+++ src/app/components/footer/footer.jsx
@@ -1,6 +1,22 @@
 import prisma from "../../../../lib/prisma";
 
 export async function Footer() {
+  //Showing view count
   const views = await prisma.counter.findUnique({ where: { id: "ws" } });
+  console.log(views);
+
+  //Updating view count
+  const updateCounter = await prisma.counter.update({
+    where: {
+      id: "ws",
+    },
+    data: {
+      views: {
+        increment: 1,
+      },
+    },
+  });
+  console.log(updateCounter);
+
   return <div className=" text-blue-500 ">{views.views}</div>;
 }
